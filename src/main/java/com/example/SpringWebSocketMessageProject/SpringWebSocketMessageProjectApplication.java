463c6feb74fcc60fd75d1d640741d12d32a58076
cengiztuglu <cngztuglu@gmail.com>
2024-05-06 16:43:15 +0300

    usingStomp

---
+++ src/main/java/com/example/SpringWebSocketMessageProject/SpringWebSocketMessageProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringWebSocketMessageProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebSocketMessageProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebSocketMessageProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringWebSocketMessageProject/SpringWebSocketMessageProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringWebSocketMessageProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebSocketMessageProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
